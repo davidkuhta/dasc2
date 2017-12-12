c385a8aa1b08cedf0f10ba3a3e679993ea0008ee
anshulsacheti <anshul.sacheti@gmail.com>
2017-12-11 19:38:38 -0500

    Added tensorboard and hyperparam tweaks

--- dasc2/agents/dqnAgent.py
+++ dasc2/agents/dqnAgent.py
@@ -11,6 +11,9 @@ from pysc2.lib import actions
 from pysc2.lib import features
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
+from tensorflow.python.ops import rnn, rnn_cell
+
+import os
 
 _NO_OP = actions.FUNCTIONS.no_op.id
 _SELECT_POINT = actions.FUNCTIONS.select_point.id
@@ -35,14 +38,14 @@ _TERRAN_BARRACKS = 21
 _NOT_QUEUED = [0]
 _QUEUED = [1]
 
-ACTION_DO_NOTHING = 'donothing'
+ACTION_NO_OP = 'no-op'
 ACTION_SELECT_SCV = 'selectscv'
 ACTION_BUILD_SUPPLY_DEPOT1 = 'buildsupplydepot1'
 ACTION_BUILD_SUPPLY_DEPOT2 = 'buildsupplydepot2'
 ACTION_BUILD_BARRACKS1 = 'buildbarracks1'
 ACTION_BUILD_BARRACKS2 = 'buildbarracks2'
 ACTION_SELECT_BARRACKS1 = 'selectbarracks1'
-ACTION_SELECT_BARRACKS2 = 'selectbarracks1'
+ACTION_SELECT_BARRACKS = 'selectbarracks'
 ACTION_BUILD_MARINE = 'buildmarine'
 ACTION_SELECT_ARMY = 'selectarmy'
 ACTION_ATTACK = 'attack'
@@ -52,7 +55,7 @@ ACTION_SCV_MINERALS = 'scvminerals'
 ACTION_SCV_VESPENE = 'scvvespene'
 
 # smart_actions = [
-#     ACTION_DO_NOTHING,
+#     ACTION_NO_OP,
 #     ACTION_SELECT_SCV,
 #     ACTION_BUILD_SUPPLY_DEPOT,
 #     ACTION_BUILD_BARRACKS1,
@@ -66,14 +69,13 @@ ACTION_SCV_VESPENE = 'scvvespene'
 # ]
 
 smart_actions = [
-    ACTION_DO_NOTHING,
+    ACTION_NO_OP,
     ACTION_SELECT_SCV,
     ACTION_BUILD_SUPPLY_DEPOT1,
     ACTION_BUILD_SUPPLY_DEPOT2,
     ACTION_BUILD_BARRACKS1,
     ACTION_BUILD_BARRACKS2,
-    ACTION_SELECT_BARRACKS1,
-    ACTION_SELECT_BARRACKS2,
+    ACTION_SELECT_BARRACKS,
     ACTION_BUILD_MARINE,
     ACTION_SELECT_ARMY,
 ]
@@ -100,8 +102,10 @@ for mm_x in range(0, 64):
 # ], dtype=np.int32)
 
 
+SELF_KILLED_UNIT_REWARD = 0
 KILL_UNIT_REWARD = 0.2
 KILL_BUILDING_REWARD = 0.5
+
 # COLLECTION_RATE_MINERALS = out["score_cumulative"][9]/100
 # COLLECTION_RATE_VESPENE = out["score_cumulative"][10]/100
 # USED_MINERALS = out["score_cumulative"][11]/100 # out["score_cumulative"][11]
@@ -110,28 +114,28 @@ KILL_BUILDING_REWARD = 0.5
 # SELF_KILLED_BUILDING_REWARD = -0.3 # Some negative reward, out["score_cumulative"][4], I think
 
 # Set learning parameters
-exploration = "boltzmann" #Exploration method. Choose between: greedy, random, e-greedy, boltzmann, bayesian.
+exploration = "bayesian" #Exploration method. Choose between: greedy, random, e-greedy, boltzmann, bayesian.
 discount = .99 #Discount factor.
 num_episodes = 20000 #Total number of episodes to train network for.
 tau = 0.001 #Amount to update target network at each step.
 batch_size = 32 #Size of training batch
 startE = 1 #Starting chance of random action
 endE = 0.1 #Final chance of random action
-annealing_steps = 200000 #How many steps of training to reduce startE to endE.
-pre_train_steps = 1500 #Number of steps used before training updates begin.
+annealing_steps = 20000000 #How many steps of training to reduce startE to endE.
+pre_train_steps = 64 #Number of steps used before training updates begin.
 
-class experience_buffer():
-    def __init__(self, buffer_size = 10000):
-        self.buffer = []
-        self.buffer_size = buffer_size
+class exp():
+    def __init__(self, size = 1000):
+        self.size = size
+        self.buf = []
 
-    def add(self,experience):
-        if len(self.buffer) + len(experience) >= self.buffer_size:
-            self.buffer[0:(len(experience)+len(self.buffer))-self.buffer_size] = []
-        self.buffer.extend(experience)
+    def query(self, size):
+        return np.reshape(np.array(random.sample(self.buf,size)),[size,4])
 
-    def sample(self,size):
-        return np.reshape(np.array(random.sample(self.buffer,size)),[size,4])
+    def append(self, replay):
+        if self.size <= len(self.buf) + len(replay):
+            self.buf[:len(replay)+len(self.buf)-self.size] = []
+        self.buf.extend(replay)
 
 def updateTargetGraph(tfVars,tau):
     total_vars = len(tfVars)
@@ -149,16 +153,25 @@ class deepRLNetwork():
         # Deep Qnetwork
         # Based off of exploration strategies (https://github.com/awjuliani/DeepRL-Agents/blob/master/Q-Exploration.ipynb)
         self.inputs = tf.placeholder(shape=[None,state_size],dtype=tf.float32)
+        tf.summary.histogram('Rewards', self.inputs)
         self.Temp = tf.placeholder(shape=None,dtype=tf.float32)
         self.keep_per = tf.placeholder(shape=None,dtype=tf.float32)
 
-        hidden = slim.fully_connected(self.inputs,64,activation_fn=tf.nn.tanh,biases_initializer=None)
+        hidden = slim.fully_connected(self.inputs,32,activation_fn=None,biases_initializer=None)
         hidden = slim.dropout(hidden,self.keep_per)
-        self.Q_out = slim.fully_connected(hidden,len(smart_actions),activation_fn=tf.nn.sigmoid,biases_initializer=None)
+        # hidden = tf.nn.rnn_cell.LSTMCell(hidden)
+        hidden = slim.fully_connected(hidden,64,activation_fn=tf.nn.tanh,biases_initializer=None)
+        hidden = slim.dropout(hidden,self.keep_per)
+        hidden = slim.fully_connected(hidden,32,activation_fn=None,biases_initializer=None)
+        hidden = slim.dropout(hidden,self.keep_per)
+        self.Q_out = slim.fully_connected(hidden,len(smart_actions),activation_fn=None,biases_initializer=None)
 
         self.predict = tf.argmax(self.Q_out,1)
         self.Q_dist = tf.nn.softmax(self.Q_out/self.Temp)
 
+        tf.summary.histogram('qOut', self.Q_out)
+        tf.summary.histogram('qPredict', self.predict)
+        # tf.summary.scalar('qDist', self.Q_dist)
 
         #Below we obtain the loss by taking the sum of squares difference between the target and prediction Q values.
         self.actions = tf.placeholder(shape=[None],dtype=tf.int32)
@@ -168,71 +181,26 @@ class deepRLNetwork():
 
         self.nextQ = tf.placeholder(shape=[None],dtype=tf.float32)
         loss = tf.reduce_sum(tf.square(self.nextQ - self.Q))
-        trainer = tf.train.GradientDescentOptimizer(learning_rate=0.0005)
-        self.updateModel = trainer.minimize(loss)
-
-    def choose_action(self, state):
-        pass
-    def learn(self, s, a, r ,s_):
-        pass
-# class Qnetwork():
-#     def __init__(self, ):
-
-# Stolen from https://github.com/MorvanZhou/Reinforcement-learning-with-tensorflow
-class QLearningTable:
-    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
-        self.actions = actions  # a list
-        self.lr = learning_rate
-        self.gamma = reward_decay
-        self.epsilon = e_greedy
-        self.q_table = pd.DataFrame(columns=self.actions)
-
-    def choose_action(self, observation):
-        self.check_state_exist(observation)
-
-        if np.random.uniform() < self.epsilon:
-            # choose best action
-            state_action = self.q_table.ix[observation, :]
-
-            # some actions have the same value
-            state_action = state_action.reindex(np.random.permutation(state_action.index))
-
-            action = state_action.argmax()
-        else:
-            # choose random action
-            action = np.random.choice(self.actions)
-
-        return action
-
-    def learn(self, s, a, r, s_):
-        self.check_state_exist(s_)
-        self.check_state_exist(s)
-
-        q_predict = self.q_table.ix[s, a]
-        q_target = r + self.gamma * self.q_table.ix[s_, :].max()
-
-        # update
-        self.q_table.ix[s, a] += self.lr * (q_target - q_predict)
-
-    def check_state_exist(self, state):
-        if state not in self.q_table.index:
-            # append new state to q table
-            self.q_table = self.q_table.append(pd.Series([0] * len(self.actions), index=self.q_table.columns, name=state))
+        tf.summary.scalar('loss', loss)
+        self.trainer = tf.train.AdagradOptimizer(learning_rate=0.0005)
+        self.updateModel = self.trainer.minimize(loss)
+        self.merged = tf.summary.merge_all()
 
 class AttackAgent(base_agent.BaseAgent):
     def __init__(self):
         super(AttackAgent, self).__init__()
 
-
         self.previous_killed_unit_score = 0
         self.previous_killed_building_score = 0
         self.previous_used_minerals = 0
+        self.previous_army_supply = 0
 
         self.previous_action = None
         self.previous_state = None
 
-        self.state_size = 20
+        self.state_size = 22
 
+        # Initializations/Tensorboard
         tf.reset_default_graph()
         self.qlearn = deepRLNetwork(smart_actions, self.state_size)
         self.target_net = deepRLNetwork(smart_actions, self.state_size)
@@ -240,9 +208,17 @@ class AttackAgent(base_agent.BaseAgent):
         self.init = tf.global_variables_initializer()
         self.trainables = tf.trainable_variables()
         self.targetOps = updateTargetGraph(self.trainables,tau)
-        self.myBuffer = experience_buffer()
+        self.myBuffer = exp(10000)
 
+        self.saver = tf.train.Saver()
         self.sess = tf.Session()
+
+        # Tensorboard
+        # self.merged = tf.summary.merge_all()
+        self.run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+        self.run_metadata = tf.RunMetadata()
+        self.train_writer = tf.summary.FileWriter('/Users/anshulsacheti/Documents/CVN/EECS_E6893/Project/tbTrain/',self.sess.graph)
+
         self.sess.run(self.init)
         updateTarget(self.targetOps,self.sess)
         self.e = startE
@@ -272,10 +248,18 @@ class AttackAgent(base_agent.BaseAgent):
         unit_type = obs.observation['screen'][_UNIT_TYPE]
 
         depot_y, depot_x = (unit_type == _TERRAN_SUPPLY_DEPOT).nonzero()
-        supply_depot_count = supply_depot_count = 1 if depot_y.any() else 0
+        if depot_x.any():
+            supply_depot_count = np.ceil((max(depot_x)-min(depot_x))/8.)
+        else:
+            supply_depot_count = 0
+        # supply_depot_count = supply_depot_count = 1 if depot_y.any() else 0
 
         barracks_y, barracks_x = (unit_type == _TERRAN_BARRACKS).nonzero()
-        barracks_count = 1 if barracks_y.any() else 0
+        if barracks_y.any():
+            barracks_count = np.ceil((max(barracks_y)-min(barracks_y))/10.)
+        else:
+            barracks_count = 0
+        # barracks_count = 1 if barracks_y.any() else 0
 
         supply_limit = obs.observation['player'][4]
         army_supply = obs.observation['player'][5]
@@ -290,6 +274,8 @@ class AttackAgent(base_agent.BaseAgent):
         current_state[1] = barracks_count
         current_state[2] = supply_limit
         current_state[3] = army_supply
+        current_state[4] = self.base_top_left
+        current_state[5] = self.total_steps
 
         hot_squares = np.zeros(16)
         enemy_y, enemy_x = (obs.observation['minimap'][_PLAYER_RELATIVE] == _PLAYER_HOSTILE).nonzero()
@@ -303,34 +289,48 @@ class AttackAgent(base_agent.BaseAgent):
             hot_squares = hot_squares[::-1]
 
         for i in range(0, 16):
-            current_state[i + 4] = hot_squares[i]
+            current_state[i + 6] = hot_squares[i]
 
         if self.previous_action is not None:
             reward = 0
 
             if killed_unit_score > self.previous_killed_unit_score:
-                reward += KILL_UNIT_REWARD
+                reward += KILL_UNIT_REWARD * (killed_unit_score - self.previous_killed_unit_score)
 
             if killed_building_score > self.previous_killed_building_score:
-                reward += KILL_BUILDING_REWARD
+                reward += KILL_BUILDING_REWARD * (killed_building_score - self.previous_killed_building_score)
 
             if total_used_minerals > self.previous_used_minerals:
-                reward += (total_used_minerals - self.previous_used_minerals)/10
+                reward += (total_used_minerals - self.previous_used_minerals)/1000
 
-            self.myBuffer.add(np.reshape(np.array([self.previous_state,self.previous_action,reward,current_state]),[1,4]))
+            if self.previous_army_supply > army_supply:
+                tmp = SELF_KILLED_UNIT_REWARD * (self.previous_army_supply - army_supply)
+                if (reward - tmp) > 0:
+                    reward -= tmp
+                else:
+                    reward = 0
+
+            self.myBuffer.append(np.reshape(np.array([self.previous_state,self.previous_action,reward,current_state]),[1,4]))
 
             if self.e > endE and self.total_steps > pre_train_steps:
                 self.e -= self.stepDrop
 
-            if self.total_steps > pre_train_steps and self.total_steps % 5 == 0:
+            if self.total_steps > pre_train_steps and self.total_steps % 10 == 0:
                 #We use Double-DQN training algorithm
-                trainBatch = self.myBuffer.sample(batch_size)
+                trainBatch = self.myBuffer.query(batch_size)
                 Q1 = self.sess.run(self.qlearn.predict,feed_dict={self.qlearn.inputs:np.vstack(trainBatch[:,3]),self.qlearn.keep_per:5.0})
                 Q2 = self.sess.run(self.target_net.Q_out,feed_dict={self.target_net.inputs:np.vstack(trainBatch[:,3]),self.target_net.keep_per:5.0})
                 doubleQ = Q2[range(batch_size),Q1]
                 targetQ = trainBatch[:,2] + (discount*doubleQ)
-                _ = self.sess.run(self.qlearn.updateModel,feed_dict={self.qlearn.inputs:np.vstack(trainBatch[:,0]),self.qlearn.nextQ:targetQ,self.qlearn.keep_per:1.0,self.qlearn.actions:trainBatch[:,1]})
+                summary, _ = self.sess.run([self.qlearn.merged, self.qlearn.updateModel],feed_dict={self.qlearn.inputs:np.vstack(trainBatch[:,0]),self.qlearn.nextQ:targetQ,self.qlearn.keep_per:5.0,self.qlearn.actions:trainBatch[:,1]},options=self.run_options,run_metadata=self.run_metadata)
                 updateTarget(self.targetOps,self.sess)
+
+                if self.total_steps >= 1000:
+                    # print("Saving model")
+                    self.train_writer.add_run_metadata(self.run_metadata, 'step%d' % self.total_steps)
+                    self.train_writer.add_summary(summary, self.total_steps)
+                    if self.total_steps % 1000 == 0:
+                        self.saver.save(self.sess, '/Users/anshulsacheti/Documents/CVN/EECS_E6893/Project/my-model')
             # self.qlearn.learn(str(self.previous_state), self.previous_action, reward, str(current_state))
 
         # rl_action = self.qlearn.choose_action(str(current_state))
@@ -362,6 +362,7 @@ class AttackAgent(base_agent.BaseAgent):
 
         smart_action = smart_actions[rl_action]
 
+        # print(smart_action)
         self.previous_killed_unit_score = killed_unit_score
         self.previous_killed_building_score = killed_building_score
         self.previous_used_minerals = total_used_minerals
@@ -374,7 +375,7 @@ class AttackAgent(base_agent.BaseAgent):
         if '_' in smart_action:
             smart_action, x, y = smart_action.split('_')
 
-        if smart_action == ACTION_DO_NOTHING:
+        if smart_action == ACTION_NO_OP:
             return actions.FunctionCall(_NO_OP, [])
 
         elif smart_action == ACTION_SELECT_SCV:
@@ -428,17 +429,7 @@ class AttackAgent(base_agent.BaseAgent):
 
                     return actions.FunctionCall(_BUILD_BARRACKS, [_NOT_QUEUED, target])
 
-        elif smart_action == ACTION_SELECT_BARRACKS1:
-            unit_type = obs.observation['screen'][_UNIT_TYPE]
-            unit_y, unit_x = (unit_type == _TERRAN_COMMANDCENTER).nonzero()
-
-            if unit_y.any():
-                loc = np.random.choice([0,20])
-                target = self.transformDistance(int(unit_x.mean()), 20, int(unit_y.mean()), loc)
-
-                return actions.FunctionCall(_SELECT_POINT, [_NOT_QUEUED, target])
-
-        elif smart_action == ACTION_SELECT_BARRACKS2:
+        elif smart_action == ACTION_SELECT_BARRACKS:
             unit_type = obs.observation['screen'][_UNIT_TYPE]
             unit_y, unit_x = (unit_type == _TERRAN_COMMANDCENTER).nonzero()
 
